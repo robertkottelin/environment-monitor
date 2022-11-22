d7440cea02ed9a535391050287c5f11963f4e807
robertkottelin <robert.kottelin@gmail.com>
2022-11-22 21:30:59 +0100

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log("get_tempdata()")
   return (
     <div className="App">
+      <button>
+        Initialize temperature tracking
+      </button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          get_tempdata()
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          get_tempdata()
+        </p>
       </header>
     </div>
   );
