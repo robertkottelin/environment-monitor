a0752c70e5890993f8836f7fd13ea47958538455
Robert Kottelin <robert.kottelin@fresenius-kabi.com>
2022-11-23 14:38:40 +0100

    autopep8

--- backend/environment-logger-main.py
+++ backend/environment-logger-main.py
@@ -2,20 +2,22 @@ import os
 import glob
 import time
 import sqlite3
- 
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
- 
+
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -28,6 +30,7 @@ def read_temp():
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c
 
+
 def get_database():
     conn = sqlite3.connect("temp.db")
     cursor = conn.cursor()
@@ -37,8 +40,9 @@ def get_database():
     conn.commit()
     return conn
 
+
 # Insert to pandas/numpy, perform elastic calculations, AI
 while True:
-	print(read_temp())	
-	get_database()
-	#time.sleep(1)
+    print(read_temp())
+    get_database()
+    # time.sleep(1)
