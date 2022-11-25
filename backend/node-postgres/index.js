d906fbc85697cbfc000d7b96b202d5bcb6673303
robertkottelin <robert.kottelin@gmail.com>
2022-11-25 20:10:10 +0100

    updates

--- backend/node-postgres/index.js
+++ backend/node-postgres/index.js
@@ -2,9 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const db = require('./queries')
 const port = 3001
-app.get('/temperatures', db.getTemperatures)
 
 app.use(bodyParser.json())
 app.use(
@@ -12,6 +10,29 @@ app.use(
     extended: true,
   })
 )
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'temperaturedb',
+    password: 'password',
+    port: 5432
+  });
+
+
+
+const getTemperatures = (request, response) => {
+    pool.connect()
+    pool.query('SELECT * FROM temperatures', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+        console.log('success')
+    });
+};
+
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
