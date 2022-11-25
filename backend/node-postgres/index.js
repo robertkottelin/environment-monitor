3ef89bc87013df621e1428ab7732de24be2dc966
robertkottelin <robert.kottelin@gmail.com>
2022-11-25 19:59:10 +0100

    trying to get the api-postgres-interface to work

--- backend/node-postgres/index.js
+++ backend/node-postgres/index.js
@@ -1,50 +1,25 @@
-const express = require('express');
-const { Client } = require("pg");
-const bodyParser = require('body-parser');
 
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
 const port = 3001
+app.get('/temperatures', db.getTemperatures)
 
-app.use(express.json())
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();
-});
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);
-
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'temperaturedb',
-  password: 'password',
-  port: 5432
-});
-
-client.connect();
-
-const getTemperatures = (request, response) => {
-  client.query('SELECT * FROM temperatures', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
+)
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' });
-});
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-app.get('/temperatures', getTemperatures);
+app.get('/temperatures', db.getTemperatures)
+
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`App running on port ${port}.`)
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,38 +3,28 @@ import "./App.css";
 // import Header from "./Header";
 import React, {useState, useEffect} from 'react';
 
+
 function App() {
   const [data, setData] = React.useState(null);
+  const [temperatures, setTemperatures] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
 
   React.useEffect(() => {
     fetch("/temperatures")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
-      // console.log(data.message)
+      .then((data) => setTemperatures(data.message));
   }, []);
   
-//   useEffect(() => {
-//     const getAPI = () => {
-//         // Local PostgreSQL Database
-//         const API = 'http://127.0.0.1:5432/';
-//         fetch(API)
-//             .then((response) => {
-//                 console.log(response);
-//                 return response.json();
-//             })
-//             .then((data) => {
-//                 console.log(data);
-//                 setLoading(false);
-//                 setApiData(data);
-//             });
-//     };
-//     getAPI();
-// }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <p>{!data ? "Loading..." : data}</p>
+        <p>{!temperatures ? "Loading..." : temperatures}</p>
       </header>
     </div>
   );
