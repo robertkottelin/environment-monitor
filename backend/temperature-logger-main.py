2aac3cfc00d25fb75f9880102ddfa10097407cb3
robertkottelin <robert.kottelin@gmail.com>
2022-11-26 10:55:33 +0100

    postgre-python working

--- backend/temperature-logger-main.py
+++ backend/temperature-logger-main.py
@@ -30,6 +30,7 @@ def read_temp_raw():
 
 # Reads temperature from raw data and converts it to Celsius (temp_c)
 def read_temp():
+    tempid = 1
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
@@ -39,24 +40,98 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         # temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c
+        tempid += 1
+        return temp_c, tempid
+
+from configparser import ConfigParser
+  
+def config(filename='/home/piro/PROJECTS/environment-monitor/backend/database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+  
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+  
+    return db
 
 # Initialize database connection, write temp to database
 def get_database():
-    conn = sqlite3.connect("temp.db")
-    cursor = conn.cursor()
+    try:
+        conn = psycopg2.connect(dbname='temperaturedb',
+                                user='postgres',
+                                password='password',
+                                host='127.0.0.1',
+                                port='5432')
+    except:
+        print('Failed to connect to db')
+    cur = conn.cursor()
     temp_raw = read_temp()
-    cursor.execute("CREATE TABLE IF NOT EXISTS TEMPERATURE(DATA NUMBER);")
-    cursor.execute("INSERT INTO TEMPERATURE VALUES (?)", (temp_raw, ))
-    conn.commit()
+    cur.execute('SELECT * FROM temperatures')
+    cur.close()
+    conn.close
+    #SQLite below
+    #cursor.execute("CREATE TABLE IF NOT EXISTS TEMPERATURE(DATA NUMBER);")
+    #cursor.execute("INSERT INTO TEMPERATURE VALUES (?)", (temp_raw, ))
+    #conn.commit()
     return conn
-
+  
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+  
+        # connect to the PostgreSQL server
+        #print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+          
+        # create a cursor
+        cur = conn.cursor()
+          
+    # execute a statement
+        #print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+  
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        #print(db_version)
+        #print('Connection successful!')
+        
+        lines = read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            # temp_f = temp_c * 9.0 / 5.0 + 32.0
+        
+        postgres_insert_query = """ INSERT INTO temperatures (celsius) VALUES (%s)"""
+        record_to_insert = ([temp_c])
+        cur.execute(postgres_insert_query, record_to_insert)
+        conn.commit()
+        cur.close()
+        conn.close()
+         
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+  
 
 # Run script on loop
 while True:
     try:
-        print(read_temp())
-        get_database()
-        # time.sleep(1)
+        connect()
+        #get_database()
+        time.sleep(2)
     except: 
         print("Something went terribly wrong...")   
