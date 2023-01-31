1f79e6ffafc11132f1f47c062e9fabe694aa1ba5
robertkottelin <robert.kottelin@gmail.com>
2023-01-31 21:01:37 +0100

    added C# project

---
+++ backend/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int number = random.Next(20, 23);
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
