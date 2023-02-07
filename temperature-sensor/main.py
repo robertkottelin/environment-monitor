72f2204a8ea68886bc21c6c37f6f48af8bbfac1f
robertkottelin <robert.kottelin@gmail.com>
2023-02-07 19:10:12 +0100

    temperature controller and MQTT communication working

--- temperature-sensor/main.py
+++ temperature-sensor/main.py
@@ -1,15 +1,14 @@
-from database import config_database, connect, insert_temperature
+# from database import config_database, connect, insert_temperature
 from read_temp import read_temperature
-from control_lamp import control_lamp
 import os
 
 def main():
-    config_database()
-    conn, cur = connect()
+    # config_database()
+    # conn, cur = connect()
     while True:
         temperature = read_temperature()
-        # print(temperature)
-        insert_temperature(conn, cur, temperature)
+        print('Sending temperature data to 192.168.0.45:', temperature)
+        # insert_temperature(conn, cur, temperature)
         os.system("mosquitto_pub -h 192.168.0.45 -t 'test_channel' -m '{}'".format(temperature))
 if __name__ == '__main__':
     main()
