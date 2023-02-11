e42875a18383bf385dbe8f3d994fd3f152604e19
robertkottelin <robert.kottelin@gmail.com>
2023-02-11 13:57:45 +0100

    updates

--- webapp2/src/App.js
+++ webapp2/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Client } from 'paho-mqtt';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,22 +11,38 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     marginTop: theme.spacing(10),
   },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 const App = () => {
   const classes = useStyles();
   const [temperature, setTemperature] = useState(0.0);
+  const [thresholdInputValue, setThresholdInputValue] = useState('');  
   const MQTT_SERVER = '192.168.0.45';
   const MQTT_PORT = 8888;
   const MQTT_TEMPERATURE_TOPIC = 'temperature_channel';
+  const MQTT_THRESHOLD_TOPIC = 'threshold_channel';
 
   useEffect(() => {
     const clientID = "clientID-" + parseInt(Math.random() * 100);
     const client = new Client(MQTT_SERVER, Number(MQTT_PORT), clientID);
 
-    client.onConnectionLost = (responseObject) => {
-      console.log('Connection lost:', responseObject.errorMessage);
-    };
+    // client.onConnectionLost = (responseObject) => {
+    //   console.log('Connection lost:', responseObject.errorMessage);
+    // };
 
     client.onMessageArrived = (message) => {
       if (message.destinationName === MQTT_TEMPERATURE_TOPIC) {
@@ -48,10 +65,45 @@ const App = () => {
     };
   }, []);
 
+  const handleThresholdSubmit = (event) => {
+    event.preventDefault();
+  
+    const clientID = "clientID-" + parseInt(Math.random() * 100);
+    const client = new Client(MQTT_SERVER, Number(MQTT_PORT), clientID);
+  
+    client.connect({
+      onSuccess: () => {
+        console.log('Connected to MQTT broker');
+        client.publish(MQTT_THRESHOLD_TOPIC, thresholdInputValue, function(error) {
+
+          if (error) {
+            console.error("Error publishing message: ", error);
+          } else {
+            console.log("Message published successfully.");
+          }
+          client.disconnect();
+        });
+      },
+      onFailure: (error) => {
+        console.log('Failed to connect to MQTT broker', error);
+      },
+    });
+  };
+  
+  
   return (
     <div className={classes.root}>
       <Typography variant="h4">Temperature:</Typography>
       <Typography variant="h2">{temperature}°C</Typography>
+  
+      <form onSubmit={handleThresholdSubmit}>
+        <input
+          type="text"
+          value={thresholdInputValue}
+          onChange={(e) => setThresholdInputValue(e.target.value)}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
